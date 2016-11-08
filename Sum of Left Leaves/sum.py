5a33f7671b0beba30f0043dd939f240d73032d0b
EvanMu96 <Senevan@outlook.com>
2016-11-08 21:27:45 +0800

    add one python solution for question404

--- Sum of Left Leaves/sum.py
+++ Sum of Left Leaves/sum.py
@@ -11,3 +11,12 @@ class Solution(object):
         :type root: TreeNode
         :rtype: int
         """
+        def sumLeftLeaves(node, part=0, left=0):
+            if node is None:
+                return 0
+            if node.left or node.right:
+                # Branch
+                return sumLeftLeaves(node.left, part, 1) + sumLeftLeaves(node.right, part, 0)
+            # Leaf
+            return node.val if left else 0
+        return sumLeftLeaves(root)
