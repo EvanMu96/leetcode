b1e91914b549b6eebb1ec787ee9dec4a6f6a957f
EvanMu96 <Senevan@outlook.com>
2016-11-16 13:06:11 +0800

    solve one problem

---
+++ Max Depth of Binary Tree/max_depth.cpp
@@ -0,0 +1,35 @@
+/**
+
+ * Definition for a binary tree node.
+
+ * struct TreeNode {
+
+ *     int val;
+
+ *     TreeNode *left;
+
+ *     TreeNode *right;
+
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+
+ * };
+
+ */
+
+class Solution {
+
+public:
+
+    int maxDepth(TreeNode* root) {
+      if (root!) return 0;
+      int leftDepth = 1;
+      int rightDepth = 1;
+      leftDepth += maxDepth(root->left);
+      rightDepth += maxDepth(root->right);
+      if (leftDepth > rightDepth)
+        return leftDepth;
+      else
+        return rightDepth;
+    }
+
+};
